c35e94e243937e4cad169122b113cae8dbdf7175
YanaMarkova <yana_markova@yahoo.com>
2023-07-10 15:19:56 +0200

    Initial commit

---
+++ src/main/java/com/example/springdataintro/services/StudentServiceImpl.java
@@ -0,0 +1,27 @@
+package com.example.springdataintro.services;
+
+import com.example.springdataintro.entities.Student;
+import com.example.springdataintro.repositories.StudentRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+
+    private StudentRepository studentRepository;
+
+    public StudentServiceImpl(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    @Override
+    public Student register(String name) {
+        Student student = new Student(name);
+        return studentRepository.save(student);
+    }
+
+    @Override
+    public Student searchByName(String name) {
+
+        return studentRepository.findByName(name);
+    }
+}
